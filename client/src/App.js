6f901ab8ac711c6ab91d7daa8ace6e650edb7719
n3ndor <n3ndor@gmail.com>
2023-06-15 17:34:21 -0400

    init react

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>CodingDojo Group Project</h2>
+      <h3>React Frontend - App.js</h3>
+    </div>
+  );
+}
+
+export default App;
