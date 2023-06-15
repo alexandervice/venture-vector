094ee7606d0b6719d87ea02e1a2a6ab79f8962ae
n3ndor <n3ndor@gmail.com>
2023-06-15 18:02:22 -0400

    testing the new branch

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h2>CodingDojo Group Project</h2>
       <h3>React Frontend - App.js</h3>
+      <h4>Created from a new branch</h4>
     </div>
   );
 }
