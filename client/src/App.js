189f9ee42cb2e10db4773ba501493613f2054a79
n3ndor <107216827+n3ndor@users.noreply.github.com>
2023-06-24 14:41:19 -0400

    logout added

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import Section from './components/Section';
 import Navbar from './components/Navbar';
 
 function App() {
-  const [user, setUser] = useState(false); // Set this value when the user logs in so we can know if they are logged in and what their info is
+  const [user, setUser] = useState(true); // Set this value when the user logs in so we can know if they are logged in and what their info is
   // if we would rather not useState for this we can also save the user info into local storage. This way we can keep them logged in until they choose to log out
   const fullPageRef = useRef();
   const [activeSection, setActiveSection] = useState(0);
@@ -12,7 +12,7 @@ function App() {
   return (
     <div className="App flex flex-col align-center justify-center text-center">
       <div className="background">
-        <Navbar fullPageApi={{ moveTo: (page) => fullPageRef.current && fullPageRef.current.moveTo(page) }} activeSection={activeSection} />
+        <Navbar user={user} setUser={setUser} fullPageApi={{ moveTo: (page) => fullPageRef.current && fullPageRef.current.moveTo(page) }} activeSection={activeSection} />
         <Fullpage ref={fullPageRef} onSectionChange={(nextIndex) => setActiveSection(nextIndex)}>
           <Section />
         </Fullpage>
