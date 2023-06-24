ad97dcb5d5708a9007b05831f389f2902ce6f4b2
n3ndor <107216827+n3ndor@users.noreply.github.com>
2023-06-24 01:36:52 -0400

    navbar rework

--- client/src/components/sections/Hero.js
+++ client/src/components/sections/Hero.js
@@ -4,7 +4,13 @@ import { Element } from 'react-scroll';
 const Hero = () => {
     return (
         <Element name='hero' className='section'>
-            <h2>Hero page</h2>
+            <div>
+                <h2>Hero page</h2>
+                <h3>VentureVector gives you the freedom to choose your own adventure and the tools to make it happen.</h3>
+                <h3>Our AI chatbot assists you in planning, while Google Maps shows you the way.</h3>
+                <h3>Explore the world at your own pace!</h3>
+            </div>
+
         </Element>
     )
 }
--- client/src/components/sections/Search.js
+++ client/src/components/sections/Search.js
@@ -1,12 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Element } from 'react-scroll';
 
 const Search = () => {
+    const [location, setLocation] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [travelerNumber, setTravelerNumber] = useState(1);
+    const [budget, setBudget] = useState(3);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // SUBMIT 
+        // OR DO SOMETHING
+    }
     return (
-        <Element name='search' className='section'>
-            <h2>Search page</h2>
+        <Element className='section search' name='search'>
+            <div>
+
+                <h2 id='search'>Search page</h2>
+
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Location:
+                        <input
+                            type="text"
+                            value={location}
+                            onChange={(e) => setLocation(e.target.value)}
+                        />
+                    </label>
+
+                    <label>
+                        Start Date:
+                        <input
+                            type="date"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                        />
+                    </label>
+
+                    <label>
+                        End Date:
+                        <input
+                            type="date"
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)}
+                        />
+                    </label>
+
+                    <label>
+                        Traveler Number:
+                        <input
+                            type="number"
+                            value={travelerNumber}
+                            onChange={(e) => setTravelerNumber(parseInt(e.target.value))}
+                        />
+                    </label>
+
+                    <label>
+                        Budget:
+                        <input
+                            type="range"
+                            min={0}
+                            max={5}
+                            value={budget}
+                            onChange={(e) => setBudget(parseInt(e.target.value))}
+                        />
+                    </label>
+
+                    <button type="submit">Submit</button>
+                </form>
+
+
+            </div>
+
+
         </Element>
     )
 }
 
-export default Search
+export default Search;
